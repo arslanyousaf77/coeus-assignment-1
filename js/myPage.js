8c85c04b55f763ff2a8fe60e93654401fdf26f1a
Arslan Yousaf <arslanyousaf77@gmail.com>
2021-04-15 10:49:25 +0500

    exp changes

---
+++ js/myPage.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 12,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
